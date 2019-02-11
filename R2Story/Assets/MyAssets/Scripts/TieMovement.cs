2e0458857132c1202224d140378ad3e8c4346fc7
Daniel Ponce <chiwisdp@gmail.com>
2019-02-11 23:55:39 +0900

    Initial follow script

---
+++ R2Story/Assets/MyAssets/Scripts/TieMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TieMovement : MonoBehaviour
+{
+  // Start is called before the first frame update
+  public GameObject Player;
+  public float movementSpeed = 4;
+  void Update()
+  {
+    transform.LookAt(Player.transform);
+    transform.position += transform.forward * movementSpeed * Time.deltaTime;
+  }
+}
